0299ff2b572f7dfe8247e60d7349313c3e5c2170
bsnodin <bsnodin@89fafa98-8746-4af2-987b-773ef2868b86>
2014-03-24 16:21:39 +0000

    pair_energy observable: Added optional arguments that specify a particular nucleotide pair whose interactions should be printed, rather than printing all possible interactions.

--- oxDNA/src/Observables/PairEnergy.cpp
+++ oxDNA/src/Observables/PairEnergy.cpp
@@ -21,6 +21,20 @@ PairEnergy<number>::~PairEnergy() {
 }
 
 template<typename number>
+void PairEnergy<number>::get_settings(input_file &my_inp, input_file &sim_inp) {
+	int tmp = 0;
+	tmp = 0;
+	_print_all_particles = true;
+	if (getInputInt(&my_inp,"particle1_id", &tmp, 0) == KEY_FOUND){
+		_print_all_particles = false;
+		_particle1_id = tmp;
+		tmp = 0;
+		getInputInt(&my_inp,"particle2_id", &tmp, 1);
+		_particle2_id = tmp;
+	}
+}
+
+template<typename number>
 std::string PairEnergy<number>::get_output_string(llint curr_step) {
 	BaseParticle<number> *p;
 	BaseParticle<number> *q;
@@ -37,16 +51,41 @@ std::string PairEnergy<number>::get_output_string(llint curr_step) {
 	for(typename std::map<int,number>::iterator l = split_energies.begin(); l != split_energies.end(); l++)
 		total_energy_diff += (*l).second;
 
-	std::vector<std::pair<BaseParticle<number> *, BaseParticle<number> *> > neighbour_pairs = this->_config_info.interaction->get_potential_interactions(this->_config_info.particles, *this->_config_info.N, *this->_config_info.box_side);
+	if (_print_all_particles){
+		std::vector<std::pair<BaseParticle<number> *, BaseParticle<number> *> > neighbour_pairs = this->_config_info.interaction->get_potential_interactions(this->_config_info.particles, *this->_config_info.N, *this->_config_info.box_side);
 
-	//printf("Total fene energy is %f \n", split_energies[0]);
-	for (int i = 0; i < (int)neighbour_pairs.size(); i++)
-	{
-		p = neighbour_pairs[i].first;
-		q = neighbour_pairs[i].second;
+		//printf("Total fene energy is %f \n", split_energies[0]);
+		for (int i = 0; i < (int)neighbour_pairs.size(); i++)
+		{
+			p = neighbour_pairs[i].first;
+			q = neighbour_pairs[i].second;
+
+			number pair_interaction = 0.;
+			bool interaction_exists = false;
+			std::stringstream pair_string;
+			pair_string << p->index << " " << q->index;
+
+			for(int k = 0; k < (int)split_energies.size(); k++)
+			{
+			   number k_th_interaction = this->_config_info.interaction->pair_interaction_term(k,q,p);
+			   pair_interaction += k_th_interaction;
+			   total_energy += k_th_interaction;
+			   pair_string << " " <<  k_th_interaction ;
+			   if(k_th_interaction != 0.f) interaction_exists = true;
+			}
+			pair_string << " " << pair_interaction << '\n';
+			if (interaction_exists) output_str << pair_string.str();
+		}
+		//number energy = this->_config_info.interaction->get_system_energy_term(RNAInteraction<number>::HYDROGEN_BONDING, this->_config_info.particles, *this->_config_info.N, *this->_config_info.box_side);
+		//energy /= *this->_config_info.N;
+		output_str << "#Total energy per particle is  " << total_energy/ *this->_config_info.N << " and should be " << total_energy_diff / *this->_config_info.N << '\n';
+		//printf("Finished, I have %s",output_str.str().c_str());
+	}
+	else{
+		p = this->_config_info.particles[_particle1_id];
+		q = this->_config_info.particles[_particle2_id];
 
 		number pair_interaction = 0.;
-		bool interaction_exists = false;
 		std::stringstream pair_string;
 		pair_string << p->index << " " << q->index;
 
@@ -56,16 +95,13 @@ std::string PairEnergy<number>::get_output_string(llint curr_step) {
 		   pair_interaction += k_th_interaction;
 		   total_energy += k_th_interaction;
 		   pair_string << " " <<  k_th_interaction ;
-		   if(k_th_interaction != 0.f) interaction_exists = true;
 		}
 		pair_string << " " << pair_interaction << '\n';
-		if (interaction_exists) output_str << pair_string.str();
+		// always print even if the interaction energy is zero
+		output_str << pair_string.str();
+		
 	}
 
-	//number energy = this->_config_info.interaction->get_system_energy_term(RNAInteraction<number>::HYDROGEN_BONDING, this->_config_info.particles, *this->_config_info.N, *this->_config_info.box_side);
-	//energy /= *this->_config_info.N;
-    output_str << "#Total energy per particle is  " << total_energy/ *this->_config_info.N << " and should be " << total_energy_diff / *this->_config_info.N << '\n';
-    //printf("Finished, I have %s",output_str.str().c_str());
     return output_str.str();
 }
 
