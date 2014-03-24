493386562676d1888cdd2f1bd4701bcba64002d2
bsnodin <bsnodin@89fafa98-8746-4af2-987b-773ef2868b86>
2014-03-24 16:21:39 +0000

    pair_energy observable: Added optional arguments that specify a particular nucleotide pair whose interactions should be printed, rather than printing all possible interactions.

--- src/Observables/PairEnergy.h
+++ src/Observables/PairEnergy.h
@@ -11,16 +11,27 @@
 #include "BaseObservable.h"
 
 /**
- * @brief Prints out all the interactions between a pair of nucleotides
+ * @brief Prints out all the interactions between all pairs of nucleotides with non-zero interaction energies (default) or between a pair of nucleotides (if specified)
+ *
+ * @verbatim
+particle1_id = <int> (particle 1 id)
+particle2_id = <int> (particle 2 id)
+@endverbatim
  */
 template<typename number>
 class PairEnergy: public BaseObservable<number> {
+protected:
+	int _particle1_id;
+	int _particle2_id;
+	bool _print_all_particles;
 
 public:
 	PairEnergy();
 	virtual ~PairEnergy();
 
 	std::string get_output_string(llint curr_step);
+
+	virtual void get_settings(input_file &my_inp, input_file &sim_inp);
 };
 
 #endif /* PAIRENERGY_H_ */
