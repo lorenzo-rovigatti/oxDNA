0d71020bc69267c51f8c5164cfef2445f6695dd3
torav <torav@89fafa98-8746-4af2-987b-773ef2868b86>
2014-05-08 14:06:24 +0000

    Fixed and improved few contrib observables.

--- oxDNA/contrib/rovigatti/Gyradius.cpp
+++ oxDNA/contrib/rovigatti/Gyradius.cpp
@@ -13,6 +13,7 @@ Gyradius<number>::Gyradius() {
 	_accumulate = false;
 	_avg_gr2 = 0;
 	_counter = 0;
+	_type = P_A;
 }
 
 template<typename number>
@@ -25,6 +26,9 @@ void Gyradius<number>::get_settings(input_file &my_inp, input_file &sim_inp) {
 	int tmp = 0;
 	getInputBoolAsInt(&my_inp, "accumulate", &tmp, 0);
 	_accumulate = (bool) tmp;
+
+	getInputInt(&my_inp, "rg_type", &_type, 0);
+	if(_type != P_A && _type != P_B) throw oxDNAException("rg_type should be either 0 (P_A) or 1 (P_B)");
 }
 
 template<typename number>
@@ -33,22 +37,26 @@ std::string Gyradius<number>::get_output_string(llint curr_step) {
 	number L = *this->_config_info.box_side;
 
 	number rg2 = 0;
+	int N_type = 0;
 	for(int i = 0; i < N; i++) {
 		BaseParticle<number> *p = this->_config_info.particles[i];
-		for(int j = i+1; j < N; j++) {
-			BaseParticle<number> *q = this->_config_info.particles[j];
-			rg2 += p->pos.sqr_min_image_distance(q->pos, L);
+		if(p->type == _type) {
+			N_type++;
+			for(int j = i+1; j < N; j++) {
+				BaseParticle<number> *q = this->_config_info.particles[j];
+				if(q->type == _type) rg2 += p->pos.sqr_min_image_distance(q->pos, L);
+			}
 		}
 	}
-	rg2 /= SQR(N);
+	rg2 /= SQR(N_type);
 
 	if(_accumulate) {
 		_avg_gr2 += rg2;
 		_counter++;
-		return Utils::sformat ("%f %d", sqrt(_avg_gr2/_counter), N);
+		return Utils::sformat ("%f %d", sqrt(_avg_gr2/_counter), N_type);
 	}
 
-	return Utils::sformat ("%f %d", sqrt(rg2), N);
+	return Utils::sformat ("%f %d", sqrt(rg2), N_type);
 }
 
 template class Gyradius<float>;
