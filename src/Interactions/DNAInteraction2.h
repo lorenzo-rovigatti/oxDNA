1f410fe7adcad5440d60fd53c9c215d9b20c8ad5
bsnodin <bsnodin@89fafa98-8746-4af2-987b-773ef2868b86>
2014-07-11 14:40:29 +0000

    Cleaned up the code for DNAInteraction2 (implementing salt) and changed the input file options so the currently that the only mandatory key is salt_concentration

--- src/Interactions/DNAInteraction2.h
+++ src/Interactions/DNAInteraction2.h
@@ -1,19 +1,21 @@
 /**
- * @brief Handles interactions between DNA nucleotides. Contains additionally interactions (mismatch repulsion, salt) with respect to DNA Interaction class
- * Implements DNA2a model - Ferdinando Version
+ * @brief Adds a Debye-Huckel (salt dependent) term to the oxDNA potential represented in DNAInteraction
  *
  * Ferdinando (April 2014) after petr (April 2014)
  * 
- * If you want to use the DNA2a model, you need to set
- *  interaction_type = DNA2a in input file
+ * To use the DNA2a model, set
+ *
+ * interaction_type = DNA2 
+ *
+ * in the input file
+ *
  * Input options:
  *
  * @verbatim
- [use_average_seq = <boolean> (defaults to yes)]
- [seq_dep_file = <string> (sets the location of the files with sequence-dependent parameters)]
- [external_model = <string> (overrides default constants for the model, set in rna_model.h), by values specified by this option)]
- [salt = <float>  (sets the salt concentration in M, defaults to 1)]
- [mismatch_repulsion = <boolean> (defaults to no)]
+ salt_concentration = <float>  (sets the salt concentration in M)
+ [dh_lambda = <float> (the value that lambda, which is a function of temperature (T) and salt concentration (I), should take when T=300K and I=1M, defaults to the value from Debye-Huckel theory, 0.3616455)]
+ [dh_strength = <float> (the value that scales the overall strength of the Debye-Huckel interaction, defaults to 0.0543)]
+ [dh_half_charged_ends = <bool>  (set to false for 2N charges for an N-base-pair duplex, defaults to 1)]
  @endverbatim
  */
 
@@ -21,43 +23,36 @@
 #define DNA2_INTERACTION_H
 
 #include "DNAInteraction.h"
-//#include "rna2_model.h"
 
 template<typename number>
 class DNA2Interaction: public DNAInteraction<number> {
 
-private:
-	number test_huckel(number rbackmod);
 protected:
-	//parameters of the interaction
 	float _salt_concentration;
 	bool _mismatch_repulsion;
 	bool _debye_huckel_half_charged_ends;
-	number _debye_huckel_prefactor; // this is the strength of the interaction
-	number _debye_huckel_lambdafactor; //Lambda is _debye_huckel_LAMBDAFACTOR / salt^0.5
+	number _debye_huckel_prefactor;
+	number _debye_huckel_lambdafactor;
 
 	//the following values are calculated
-	number _debye_huckel_Vrc; // is equal to DH(2*lambda,lambda,prefactor)
-    number _debye_huckel_RC; //this is the maximum interaction distance between backbones to interact with DH
-	number _debye_huckel_B; //prefactor of the quadratic cut-off
-	number _debye_huckel_RHIGH; //distance after which the potential is replaced by a quadratic cut-off
-	number _minus_kappa; //= -1/lambda
+	number _debye_huckel_RC; // this is the maximum interaction distance between backbones to interact with DH
+	number _debye_huckel_RHIGH; // distance after which the potential is replaced by a quadratic cut-off
+	number _debye_huckel_B; // prefactor of the quadratic cut-off
+	number _minus_kappa;
 
-    virtual number _debye_huckel(BaseParticle<number> *p, BaseParticle<number> *q, LR_vector<number> *r, bool update_forces);
+	virtual number _debye_huckel(BaseParticle<number> *p, BaseParticle<number> *q, LR_vector<number> *r, bool update_forces);
 
 public:
-	enum {
-	  DEBYE_HUCKEL = 7
+        enum {
+		DEBYE_HUCKEL = 7
 	};
-	DNA2Interaction();    // Constructor
-	virtual ~DNA2Interaction() {} // Destructor
-
+	DNA2Interaction();
+	virtual ~DNA2Interaction() {}
+    
 	virtual number pair_interaction_nonbonded(BaseParticle<number> *p, BaseParticle<number> *q, LR_vector<number> *r=NULL, bool update_forces=false);
 
-	virtual void get_settings(input_file &inp); //get settings from input file
-	virtual void init(); // initialisation
-
-
+	virtual void get_settings(input_file &inp);
+	virtual void init();
 };
 
 #endif
