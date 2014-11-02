c49679a7b4e239a49eaac8df146992463490a09a
torav <torav@89fafa98-8746-4af2-987b-773ef2868b86>
2014-11-02 22:34:23 +0000

    1) Bit of code clean up (removal of old comments etc.) 2) Update of the LJ interaction: it can be now used to simulate generic n-2n potentials 3) Update of an interaction for the simulation of patchy particles + polymers

--- oxDNA/src/Interactions/LJInteraction.cpp
+++ oxDNA/src/Interactions/LJInteraction.cpp
@@ -13,6 +13,7 @@ LJInteraction<number>::LJInteraction() : BaseInteraction<number, LJInteraction<n
 	_is_ka_mixture = false;
 	_sigma[0] = _sigma[1] = _sigma[2] = 1.;
 	_epsilon[0] = _epsilon[1] = _epsilon[2] = 1.;
+	_n[0] = _n[1] = _n[2] = 6;
 	_N_A = _N_B = 0;
 }
 
@@ -25,19 +26,28 @@ template<typename number>
 void LJInteraction<number>::get_settings(input_file &inp) {
 	IBaseInteraction<number>::get_settings(inp);
 
-	float rcut = 2.5f;
-	getInputFloat(&inp, "LJ_rcut", &rcut, 0);
-	this->_rcut = (number) rcut;
+	getInputInt(&inp, "LJ_n", _n, 0);
+	_n[1] = _n[2] = _n[0];
+	// no odd exponents allowed
+	if((_n[0] % 2) == 1) throw oxDNAException("LJ_n must be an even integer");
 
 	int ka = 0;
 	getInputBoolAsInt(&inp, "LJ_kob_andersen", &ka, 0);
 	_is_ka_mixture = (bool) ka;
+
+	double sigma;
+	if(getInputDouble(&inp, "LJ_sigma[2]", &sigma, 0) == KEY_FOUND) {
+		_sigma[2] = sigma;
+		_sigma[1] = 0.5*(1. + _sigma[2]);
+	}
+
+	float rcut = 2.5f;
+	getInputFloat(&inp, "LJ_rcut", &rcut, 0);
+	this->_rcut = (number) rcut;
 }
 
 template<typename number>
 void LJInteraction<number>::init() {
-	this->_sqr_rcut = SQR(this->_rcut);
-
 	if(_is_ka_mixture) {
 		_sigma[1] = 0.8;
 		_sigma[2] = 0.88;
@@ -48,9 +58,12 @@ void LJInteraction<number>::init() {
 	for(int i = 0; i < 3; i++) {
 		number rcut = this->_rcut * _sigma[i];
 		_sqr_LJ_rcut[i] = SQR(rcut);
-		_E_cut[i] = 4. * _epsilon[i] * (pow(_sigma[i]/rcut, (number)12.) - pow(_sigma[i]/rcut, (number)6.));
+		_E_cut[i] = 4. * _epsilon[i] * (pow(_sigma[i]/rcut, (number)2*_n[i]) - pow(_sigma[i]/rcut, (number)_n[i]));
 		_sqr_sigma[i] = SQR(_sigma[i]);
 	}
+
+	if(_sigma[2] > _sigma[0]) this->_rcut *= _sigma[2];
+	this->_sqr_rcut = SQR(this->_rcut);
 }
 
 template<typename number>
@@ -60,23 +73,20 @@ void LJInteraction<number>::allocate_particles(BaseParticle<number> **particles,
 
 template<typename number>
 void LJInteraction<number>::read_topology(int N, int *N_strands, BaseParticle<number> **particles) {
-	if(!_is_ka_mixture) BaseInteraction<number, LJInteraction<number> >::read_topology(N, N_strands, particles);
-	else {
-		*N_strands = N;
+	*N_strands = N;
 
-		std::ifstream topology(this->_topology_filename, ios::in);
-		if(!topology.good()) throw oxDNAException("Can't read topology file '%s'. Aborting", this->_topology_filename);
-		char line[512];
-		topology.getline(line, 512);
-		topology.close();
-		sscanf(line, "%*d %d\n", &_N_B);
-		_N_A = N - _N_B;
+	std::ifstream topology(this->_topology_filename, ios::in);
+	if(!topology.good()) throw oxDNAException("Can't read topology file '%s'. Aborting", this->_topology_filename);
+	char line[512];
+	topology.getline(line, 512);
+	topology.close();
+	sscanf(line, "%*d %d\n", &_N_B);
+	_N_A = N - _N_B;
 
-		allocate_particles(particles, N);
-		for (int i = 0; i < N; i ++) {
-		   particles[i]->index = particles[i]->strand_id = i;
-		   particles[i]->type = particles[i]->btype = (i < _N_A) ? P_A : P_B;
-		}
+	allocate_particles(particles, N);
+	for (int i = 0; i < N; i ++) {
+	   particles[i]->index = particles[i]->strand_id = i;
+	   particles[i]->type = particles[i]->btype = (i < _N_A) ? P_A : P_B;
 	}
 }
 
