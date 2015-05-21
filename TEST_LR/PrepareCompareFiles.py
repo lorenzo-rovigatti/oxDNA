d86e7c10066a00f82191ff01ae11743e372aa0ff
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2015-05-21 14:24:21 +0000

    Implemented a new test suite. Since this is its first implementaton I expect it to be ridden with bugs. You can find a brief(ish) introduction and tutorial in the TEST_LR/howto.txt file.

---
+++ TEST_LR/PrepareCompareFiles.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+from TestSuite import *
+
+class CompareFileGenerator(object):
+    def __init__(self, folder, oxDNA_path, level):
+        self.log_prefix = "CompareFileGenerator:"
+        
+        self.base_folder = os.getcwd()
+        self.folder = folder
+        self.oxDNA_path = os.path.abspath(oxDNA_path)
+        self.level = level
+            
+        input_name = level + SUFFIX_INPUT
+        input_path = os.path.join(folder, input_name)
+        if os.path.exists(input_path) and os.path.isfile(input_path):
+            self.system = System(folder, level) 
+                
+    def launch(self):
+        os.chdir(self.folder)
+        
+        to_execute = "%s %s" % (self.oxDNA_path, self.system.input_name)
+        p = sp.Popen(to_execute, shell=True, stdout=sp.PIPE, stderr=sp.PIPE)
+        p.wait()
+        
+        self.system.simulation_done(p, False)
+        
+        os.chdir(self.base_folder)
+        
+    def print_compare_file(self):
+        self.system.analyser.print_compare_file()
+    
+
+def main():
+    def print_usage():
+        print "USAGE:"
+        print "\t%s path oxDNA_executable test_level [-d|--debug] [-h|--help] [-v|--version]" % sys.argv[0]
+        exit(1)
+
+    def print_version():
+        print "oxDNA Test Suite v 0.1"
+        print "Copyright (C) 2015 oxDNA"
+        print "This is free software; see the source for copying conditions.  There is NO"
+        print "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n"
+        exit(1)
+
+    shortArgs = 'dhv'
+    longArgs = ['debug', 'help', 'version']
+    
+    try:
+        import getopt
+        args, files = getopt.gnu_getopt(sys.argv[1:], shortArgs, longArgs)
+        for k in args:
+            if k[0] == '-d' or k[0] == '--debug': Logger.debug_level = 0
+            if k[0] == '-h' or k[0] == '--help': print_usage()
+            if k[0] == '-v' or k[0] == '--version': print_version()
+            
+    except Exception as e:
+        print e
+        print_usage()
+        
+    if len(sys.argv) < 4:
+        print_usage()
+        exit(1)
+        
+    generator = CompareFileGenerator(sys.argv[1], sys.argv[2], sys.argv[3])
+    generator.launch()
+    generator.print_compare_file()
+
+if __name__ == '__main__':
+    main()
