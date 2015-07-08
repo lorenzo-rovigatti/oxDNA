e22cd8e71bf93888facce1ad5ef1f476881b021b
Ferdinando Randisi <ferdinando.randisi@gmail.com>
2015-07-08 16:08:02 +0000

    Fixed a bug in the observable VectorAngle. Hopefully it works now. --this line, and those below, will be ignored--
    M    VectorAngle.cpp
    M    VectorAngle.h

--- src/Observables/VectorAngle.h
+++ src/Observables/VectorAngle.h
@@ -1,49 +1,47 @@
 /*
- * MeanVectorCosine.h
+ * VectorAngle.h
  *
  *  Created on: Dec 9, 2014
  *      Author: Ferdinando Randisi
  */
 
-#ifndef MEANVECTORCOSINE_H_
-#define MEANVECTORCOSINE_H_
+#ifndef VECTORANGLE_H_
+#define VECTORANGLE_H_
 
 #include "BaseObservable.h"
 /**
- * @brief Outputs either the mean cosine of the angle between one of the orientation
- * 				vectors of neighbouring particles, or the quantity
- * 				(v2*v2')(v3*v3') - |v2 ^ v2||v3 ^ v3|
- * 				where vi and vi' are the orientation vectors of neighbouring particles.
+ * @brief Outputs either the cosine of the angle between one of the orientation
+ * 				vectors of neighbouring particles, or the twist angle in the TEP model, namely
+ * 				TODO: write the awful thing
+ * 				where u and u' are the longitudinal orientation vectors of neighbouring beads (T.v1 and T.v1')
+ * 				and f and f' are the other two orientation vectors.
  *
- * To use this observable, use type = mean_vector_cosine
+ * To use this observable, use type = vector_angle
  *
  * This observable takes one mandatory argument and three optional arguments
  * @verbatim
-chain_id = <int> (chain id)
-first_particle_position = <int> (defaults to 0. position along the chain of  the first particle on which to compute the vector's cosine with the next particle)
-last_particle_position = <int> (defaults to N-2, where N is the number of elements of the chain. Position along the chain of the last particle over which to compute the vector's cosine with the next particle)
-vector_to_average = <int> (defaults to 1. Can be 1,2, or 3 depending on the vectors we wish to consider, or 0. In that case it measures the quantity (v2*v2')(v3*v3') - |v2 ^ v2||v3 ^ v3|)
+first_particle_index = <int> (defaults to 0. index of the first particle on which to compute the angle with the next particle)
+last_particle_index = <int> (defaults to the index of the first-but last bead in the same strand as the first particle. Therefore, if I have a strand of N beads, the last one will be the one with index N-2. This is because the last bead is atypical in the TEP model (e.g. it's aligned with the vector before it rather than the one in front of it.). index of the last particle of the chain on which to compute the angle. )
+angle_index = <int> (defaults to 1. Can be 1,2, or 3 depending on the vectors we wish to consider, or 0. In that case it measures the twisting angle, as defined above. Only the twisting angle can be negative, all the others will be always taken positive.
 @endverbatim
  */
 
 template<typename number>
-class MeanVectorCosine : public BaseObservable<number> {
+class VectorAngle : public BaseObservable<number> {
 private:
 	// arguments
 	int _chain_id;
-	int _first_particle_position;
-	int _last_particle_position;
-	int _vector_to_average;
+	int _first_particle_index;
+	int _last_particle_index;
+	int _angle_index;
 	// indices of the first and last particle in the subchain to average
 	int _first_particle_id;
 	int _last_particle_id;
-	number _one_over_number_of_values;
 	//ausiliary variables to compute the cosine - declared here so that
 	// they are not declared every time the function get_output_string is called.
-	LR_vector<number> _u, _up, _v, _vp, _f, _fp;
 public:
-	MeanVectorCosine();
-	virtual ~MeanVectorCosine();
+	VectorAngle();
+	virtual ~VectorAngle();
 
 	virtual void init(ConfigInfo<number> &config_info);
 	virtual void get_settings(input_file &my_inp, input_file &sim_inp);
@@ -52,4 +50,4 @@ public:
 	void set_first_last_particle_id();
 };
 
-#endif /* MEANVECTORCOSINE_H_ */
+#endif /* VECTORANGLE_H_ */
