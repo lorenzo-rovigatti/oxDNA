c54b690aceabe2a9506162e45eed207eedeac69d
flavioromano <flavioromano@89fafa98-8746-4af2-987b-773ef2868b86>
2018-10-24 16:03:29 +0000

    added new interactions and moves in contrib/romano

---
+++ oxDNA/contrib/romano/src/Backends/MCMoves/Reappear.h
@@ -0,0 +1,34 @@
+/**
+ * @file    Reappear.h
+ * @date    20/ott/2018
+ * @author  flavio
+ *
+ *
+ */
+
+#ifndef REAPPEAR_H_
+#define REAPPEAR_H_
+#define MCMOVE_CUSTOM
+
+#include "../../../../../src/Backends/MCMoves/BaseMove.h"
+
+template<typename number>
+class Reappear : public BaseMove<number> {
+	protected:
+
+		LR_vector<number> pos_old;
+
+	public:
+		Reappear();
+		virtual ~Reappear();
+
+		void apply (llint curr_step);
+		virtual void get_settings(input_file &inp, input_file &sim_inp);
+		virtual void init(void);
+};
+
+
+extern "C" BaseMove<float> * make_Reappear_float()   { return new Reappear<float> () ; }
+extern "C" BaseMove<double> * make_Reappear_double() {return new Reappear<double> () ; }
+
+#endif // REAPPEAR_H_
