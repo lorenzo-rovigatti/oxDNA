7fd8c1c4859b33068833709871390a0ef138ceb9
torav <torav@89fafa98-8746-4af2-987b-773ef2868b86>
2019-06-27 11:37:44 +0000

    Clean up the source tree and update the changelog

--- oxDNA/src/Observables/HBList.h
+++ oxDNA/src/Observables/HBList.h
@@ -13,11 +13,9 @@
  * 
  * @verbatim
 type = hb_list (name of  the observable)
-only_count = <bool> (if True, don't report the detailed binding profile but just count the bonds. Defaults to False.)
-measure_mean_shift = <bool> (if True, measure the mean absolute shift in base-pair register, as described below. Defaults to false.
-
-                     with this option, we compute the distances between each unpaired base and a domain of length 2*_max_shift+1 bases on the other strand, centred on the base in complementary position. The absolute topological displacement between the closes base and the complementary base is then reported: for example, if an unpaired base is actually closest to a base that is 3 basepairs ahead of its complementary basepair, its shift will be 3, assuming that max_shift (see below) is 3 or more. The threshold is supposed to prevent accounting for plectoneme loops, so shouldn't be too much, but should also ultimately be greater than most values measured.
-max_shift = 5
+only_count = <bool> (if True, don't report the detailed binding profile but just count the bonds. Defaults to false.)
+measure_mean_shift = <bool> (if True, measure the mean absolute shift in base-pair register, as described below. Defaults to false.)
+max_shift = 5 (with this option, we compute the distances between each unpaired base and a domain of length 2*_max_shift+1 bases on the other strand, centred on the base in complementary position. The absolute topological displacement between the closes base and the complementary base is then reported: for example, if an unpaired base is actually closest to a base that is 3 basepairs ahead of its complementary basepair, its shift will be 3, assuming that max_shift (see below) is 3 or more. The threshold is supposed to prevent accounting for plectoneme loops, so shouldn't be too much, but should also ultimately be greater than most values measured.)
 @endverbatim
  */
 #ifndef HBLIST_H_
