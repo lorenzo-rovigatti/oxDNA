4279917436b97c88802887bba9381dab83a66031
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2019-10-24 17:37:49 +0200

    Fix to the previous commit

--- src/CUDA/Thermostats/CUDAThermostatFactory.cu
+++ src/CUDA/Thermostats/CUDAThermostatFactory.cu
@@ -8,10 +8,10 @@
 #include "CUDAThermostatFactory.h"
 
 #include "CUDANoThermostat.h"
-/*#include "CUDABrownianThermostat.h"
- #include "CUDALangevinThermostat.h"
- #include "CUDASRDThermostat.h"
- #include "CUDABussiThermostat.h"*/
+#include "CUDABrownianThermostat.h"
+#include "CUDALangevinThermostat.h"
+#include "CUDASRDThermostat.h"
+#include "CUDABussiThermostat.h"
 #include "../../Utilities/Utils.h"
 
 CUDABaseThermostat*CUDAThermostatFactory::make_thermostat(input_file &inp, BaseBox * box) {
@@ -19,13 +19,13 @@ CUDABaseThermostat*CUDAThermostatFactory::make_thermostat(input_file &inp, BaseB
 	char thermostat_type[512] = "no";
 	getInputString(&inp, "thermostat", thermostat_type, 0);
 
-	/*if(!strncmp(thermostat_type, "john", 512)) return new CUDABrownianThermostat();
-	 else if(!strncmp(thermostat_type, "brownian", 512)) return new CUDABrownianThermostat();
-	 else if(!strncmp(thermostat_type, "bussi", 512)) return new CUDABussiThermostat();
-	 else if(!strncmp(thermostat_type, "langevin", 512)) return new CUDALangevinThermostat();
-	 else if(!strncmp(thermostat_type, "srd", 512) || !strncmp(thermostat_type, "SRD", 512)) {
-	 return new CUDASRDThermostat(box);
-	 }
-	 else */if(!strncmp(thermostat_type, "no", 512)) return new CUDANoThermostat();
+	if(!strncmp(thermostat_type, "john", 512)) return new CUDABrownianThermostat();
+	else if(!strncmp(thermostat_type, "brownian", 512)) return new CUDABrownianThermostat();
+	else if(!strncmp(thermostat_type, "bussi", 512)) return new CUDABussiThermostat();
+	else if(!strncmp(thermostat_type, "langevin", 512)) return new CUDALangevinThermostat();
+	else if(!strncmp(thermostat_type, "srd", 512) || !strncmp(thermostat_type, "SRD", 512)) {
+		return new CUDASRDThermostat(box);
+	}
+	else if(!strncmp(thermostat_type, "no", 512)) return new CUDANoThermostat();
 	else throw oxDNAException("Invalid CUDA thermostat '%s'", thermostat_type);
 }
