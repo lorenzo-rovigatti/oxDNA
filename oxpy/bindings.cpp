89f9ca5fc233076ebbec4f56bd847f7261a24c82
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2020-01-16 16:58:52 +0100

    Add some Python bindings

--- oxpy/bindings.cpp
+++ oxpy/bindings.cpp
@@ -10,9 +10,44 @@
 #include "OxpyContext.h"
 #include "OxpyManager.h"
 
+#include <Particles/BaseParticle.h>
+#include <Interactions/BaseInteraction.h>
+#include <Utilities/ConfigInfo.h>
+
+void export_BaseParticle(py::module &m);
+void export_IBaseInteraction(py::module &m);
+void export_ConfigInfo(py::module &m);
+
 PYBIND11_MODULE(oxpy, m) {
 	export_OxpyContext(m);
 
 	export_SimManager(m);
 	export_OxpyManager(m);
+
+	export_BaseParticle(m);
+//	export_IBaseInteraction(m);
+	export_ConfigInfo(m);
+}
+
+void export_BaseParticle(py::module &m) {
+	pybind11::class_<BaseParticle, std::shared_ptr<BaseParticle>> particle(m, "BaseParticle");
+
+	particle
+		.def(py::init<>());
+}
+
+void export_IBaseInteraction(py::module &m) {
+	pybind11::class_<IBaseInteraction, std::shared_ptr<IBaseInteraction>> interaction(m, "IBaseInteraction");
+
+	interaction
+		.def(py::init<>());
+}
+
+void export_ConfigInfo(py::module &m) {
+	pybind11::class_<ConfigInfo, std::shared_ptr<ConfigInfo>> conf_info(m, "ConfigInfo");
+
+	conf_info
+//		.def_readwrite("particles", &ConfigInfo::particles)
+		.def_readwrite("interaction", &ConfigInfo::interaction)
+		.def_readwrite("N", &ConfigInfo::N);
 }
