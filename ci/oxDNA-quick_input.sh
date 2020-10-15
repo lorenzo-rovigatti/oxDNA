0c903592968f2c938c9c3d31a0936e8f712f802a
Debesh-XPS <debesh.mandal@hotmail.com>
2020-10-15 17:26:29 +0100

    fixed bash script

--- ci/oxDNA-quick_input.sh
+++ ci/oxDNA-quick_input.sh
@@ -1,15 +1,34 @@
 #!/usr/bin/bash
-OXDNA_BIN=./build/bin/oxDNA
 
-$OXDNA_BIN test/DNA/DSDNA8/MC/quick_input
-$OXDNA_BIN test/DNA/DSDNA8/MD/quick_input
-$OXDNA_BIN test/DNA/DSDNA8/VMMC/quick_input
-$OXDNA_BIN test/DNA/HAIRPIN18/VMMC_DOUBLE/quick_input
-$OXDNA_BIN test/DNA/HAIRPIN18/VMMC_DOUBLE/quick_input
-$OXDNA_BIN test/DNA/SSDNA15/MC/quick_input
-$OXDNA_BIN test/DNA/SSDNA15/MD/quick_input
-$OXDNA_BIN test/DNA/SSDNA15/VMMC/quick_input
-$OXDNA_BIN test/LJ/quick_input
-$OXDNA_BIN test/THERMOSTATS/BUSSI/quick_input
-$OXDNA_BIN test/THERMOSTATS/JOHN/quick_input
-$OXDNA_BIN test/THERMOSTATS/LANGEVIN/quick_input
+# List of targets
+TARGETS=( \
+    test/DNA/DSDNA8/MC \
+    test/DNA/DSDNA8/MD \
+    test/DNA/DSDNA8/VMMC \
+    test/DNA/HAIRPIN18/VMMC_DOUBLE \
+    test/DNA/HAIRPIN18/VMMC_DOUBLE \
+    test/DNA/SSDNA15/MC \
+    test/DNA/SSDNA15/MD \
+    test/DNA/SSDNA15/VMMC \
+    test/LJ \
+    test/THERMOSTATS/BUSSI \
+    test/THERMOSTATS/JOHN \
+    test/THERMOSTATS/LANGEVIN \
+)
+
+# Find oxDNA and link to temporary directory
+ln $(find . -name oxDNA) /tmp/oxDNA
+OXDNA_BIN=/tmp/oxDNA
+echo "OXDNA_BIN: ${OXDNA_BIN}"
+
+# Run all of the test scripts
+for target in ${TARGETS[@]}; do
+pushd $target >& /dev/null
+echo "RUNNING: ${target}"
+$OXDNA_BIN quick_input
+popd >& /dev/null
+echo "FINISHED!"
+done
+
+# remove the linker
+rm /tmp/oxDNA
