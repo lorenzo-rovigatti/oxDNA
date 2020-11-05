c419b49d4cc54be8aa26b7b4ce7df4f149b7dffc
Debesh Mandal <debesh.mandal13@imperial.ac.uk>
2020-11-05 12:19:51 +0000

    uninstalling can take place successfully

--- oxpy/setup.py
+++ oxpy/setup.py
@@ -5,7 +5,7 @@ if sys.version_info < (3,0):
     sys.exit('Error, Python < 3.0 is not supported')
 
 setup(
-    name = 'oxpy-oxdna',
+    name = 'oxpy',
     version = '${revision}',
     packages = [ 'oxpy' ],
     package_dir = {
