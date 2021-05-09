9ebb1426d7620ef9f78b4b47d79f0453e8fbbfec
rahul2512 <rs25.iitr@gmail.com>
2021-05-09 15:38:35 +0200

    updated VMMC_CPUBackend.cpp

--- src/Backends/VMMC_CPUBackend.cpp
+++ src/Backends/VMMC_CPUBackend.cpp
@@ -6,7 +6,9 @@
 #include "VMMC_CPUBackend.h"
 #include "../Utilities/Utils.h"
 #include "../Interactions/RNAInteraction2.h"
-#include "../Interactions/DNA2Interaction.h"
+
+//#include "../Interactions/DNA2Interaction.h"
+#include "../Interactions/DNA3Interaction.h"
 
 #include <set>
 #include <sstream>
@@ -187,6 +189,7 @@ void VMMC_CPUBackend::get_settings(input_file & inp) {
 	std::vector<std::string> ok_interactions; //= {"DNA", "DNA_nomesh", "DNA2", "DNA2_nomesh","RNA","RNA2"};//would work in C++11 and we woudln't to push back the elements
 	ok_interactions.push_back("DNA");
 	ok_interactions.push_back("DNA2");
+        ok_interactions.push_back("DNA3");
 	ok_interactions.push_back("DNA2ModInteraction");
 	ok_interactions.push_back("DNA_nomesh");
 	ok_interactions.push_back("DNA2_nomesh");
