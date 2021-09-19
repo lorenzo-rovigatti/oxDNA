63afffa96d6e899e46c08b58071ab4f07276a836
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2021-09-19 18:55:25 +0200

    Make it compile and remove warnings on mac os X

--- src/Utilities/Utils.h
+++ src/Utilities/Utils.h
@@ -44,13 +44,13 @@ inline number sum(number *v, int N) {
 
 // trim from start
 inline std::string &ltrim(std::string &s) {
-	s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+	s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c) { return !std::isspace(c); }));
 	return s;
 }
 
 // trim from end
 inline std::string &rtrim(std::string &s) {
-	s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+	s.erase(std::find_if(s.rbegin(), s.rend(), [](int c) { return !std::isspace(c); }).base(), s.end());
 	return s;
 }
 
