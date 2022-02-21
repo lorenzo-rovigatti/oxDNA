4dbc3dc9805c000cdafd57d6960575d6ba9daae3
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2022-02-21 12:39:51 +0100

    Fix bugs linked to oxpy
    * Fix a bug that prevented compilation on systems with python2 and
    python3 installed
    * Fix a bug that prevented the generation of the python bindings
    documentation (see
    [here](https://github.com/sphinx-doc/sphinx/issues/9049))

--- docs/oxpy/html/_static/documentation_options.js
+++ docs/oxpy/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.0a1.dev198+gaf139b1.d20210217',
+    VERSION: '3.0.0a1.dev246+ge05314f.d20220221',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
