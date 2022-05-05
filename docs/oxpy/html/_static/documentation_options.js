466241727efa881740b8f9793f68ce386d331091
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2022-05-05 23:21:08 +0200

    Add the reference file for the oxpy test and update the docs

--- docs/oxpy/html/_static/documentation_options.js
+++ docs/oxpy/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.0.0a1.dev263+g1359de7.d20220428',
+    VERSION: '3.2.2.dev47+g785a315.d20220505',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
