4b25590a9d45cc811bc3eaf608502bbca4678a65
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2023-01-19 16:22:21 +0100

    Fix the oxDNA exe path in some examples
    This is in response to one of the issues raised during the [JOSS
    review](https://github.com/openjournals/joss-reviews/issues/4693#issuecomment-1384241761)

--- examples/RELAX_INITIAL_CONFIGURATION/run.sh
+++ examples/RELAX_INITIAL_CONFIGURATION/run.sh
@@ -2,7 +2,7 @@
 
 # run the relaxation procedure
 
-../../bin/oxDNA input_weak_constant
+../../build/bin/oxDNA input_weak_constant
 mv last_conf.dat relaxed_weak.conf
-../../bin/oxDNA input_strong_constant
+../../build/bin/oxDNA input_strong_constant
 mv last_conf.dat relaxed.conf
