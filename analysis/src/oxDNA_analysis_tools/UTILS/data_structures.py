e46a8623a9cd72a355441a0b5f142186b1efc017
Erik Poppleton <erikpoppleton@bpe-mac-008w.local>
2023-02-09 13:02:24 +0100

    added get_lengh member function to Strand

--- analysis/src/oxDNA_analysis_tools/UTILS/data_structures.py
+++ analysis/src/oxDNA_analysis_tools/UTILS/data_structures.py
@@ -141,6 +141,9 @@ class Strand:
 
     def is_circular(self):
         return True if self.monomers[0].n3 == self.monomers[-1].id else False
+    
+    def get_length(self):
+        return len(self.monomers)
 
 #No, you cannot make n3, n5 and pair refs to other Monomers
 #Scaffold strands are long enough that it would stack overflow while pickling for Pool processing
