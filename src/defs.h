689be17d94b63705a8f295c3a4bead66c42917c5
Eryk Ratajczyk <77619269+eryykr@users.noreply.github.com>
2023-03-10 01:03:37 +0000

    Update defs.h

--- src/defs.h
+++ src/defs.h
@@ -33,6 +33,30 @@
 #define N_T 3
 #define N_DUMMY 4
 
+//Amino Acids
+#define A_A (-1)
+#define A_R (-2)
+#define A_N (-3)
+#define A_D (-4)
+#define A_C (-5)
+#define A_E (-6)
+#define A_Q (-7)
+#define A_G (-8)
+#define A_H (-9)
+#define A_I (-10)
+#define A_L (-11)
+#define A_K (-12)
+#define A_M (-13)
+#define A_F (-14)
+#define A_P (-15)
+#define A_S (-16)
+#define A_T (-17)
+#define A_W (-18)
+#define A_Y (-19)
+#define A_V (-20)
+#define A_DUMMY (-21)
+
+
 #include "model.h"
 #include "Utilities/LR_vector.h"
 #include "Utilities/LR_matrix.h"
