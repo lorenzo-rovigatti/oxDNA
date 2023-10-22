268a298ca33b30610d137f7e3de05cab2f9b3750
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2023-10-21 22:50:20 -0700

    Added new interactions

---
+++ src/Interactions/PHBInteraction.h
@@ -0,0 +1,38 @@
+// Patchy Helix Bundles
+
+#ifndef PHBInteraction_H_
+#define PHBInteraction_H_
+#include "BaseInteraction.h"
+
+class PHBInteractions: public BaseInteraction{
+public:
+    int particleNum,strands,totHelix,totPatchy;
+
+protected:
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
