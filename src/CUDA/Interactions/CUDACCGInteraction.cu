1708716d881f7968e9792c166c1da408b839176a
Subhajit-Roy-Partho <me.subhajitroy1999@gmail.com>
2023-12-25 21:38:21 -0700

    update

--- src/CUDA/Interactions/CUDACCGInteraction.cu
+++ src/CUDA/Interactions/CUDACCGInteraction.cu
@@ -1,4 +1,5 @@
 #include "CUDACCGInteration.h"
+#include "../cuda_utils/CUDA_lr_common.cuh"
 
 CUDACCGInteraction::CUDACCGInteraction(){
 
@@ -27,9 +28,24 @@ void CUDACCGInteraction::compute_forces(CUDABaseList *lists, c_number4 *d_poss, 
 
 __device__ void repulsive(c_number prefactor,c_number4 &r, c_number4 &F, c_number sigma, c_number rstar, c_number b, c_number rc){
     c_number rnorm = CUDA_DOT(r,r);
+    F.x = F.y = F.z = F.w = (c_number) 0.f;
     if(rnorm < SQR(rc)){
         if(rnorm > SQR(rstar)){
-            rmod = 
+            c_number rmod = sqrt(rnorm);
+            c_number rrc = rmod -rc;
+            c_number fmod = 2.f*prefactor*b*rrc/rmod;
+            F.x = r.x * fmod;
+            F.y = r.y * fmod;
+            F.z = r.z * fmod;
+            F.w = prefactor*b*SQR(rrc);
+            return;
         }
+        c_number lj_part = CUB(SQR(sigma)/rnorm);
+        c_number fmod = 24.f * prefactor * (lj_part - 2.f * SQR(lj_part)) / rnorm;
+        F.x = r.x * fmod;
+		F.y = r.y * fmod;
+		F.z = r.z * fmod;
+        F.w = 4.f * prefactor * (SQR(lj_part) - lj_part);
+        return;
     }
 }
