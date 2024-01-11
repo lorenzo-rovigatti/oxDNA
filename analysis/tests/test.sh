ffd1dd9c1db69d8ea5c95185269181dd6dea8234
Lorenzo Rovigatti <lorenzo.rovigatti@gmail.com>
2024-01-11 16:42:30 +0100

    Fix #89
    Fix the way std::string::compare is used when comparing strings to "NA" and "NA_relax".
    This commit also add oat's test suite to the commands launched by make test.

--- analysis/tests/test.sh
+++ analysis/tests/test.sh
@@ -1,175 +1,78 @@
 #!/bin/bash
 
-echo "Testing align..."
-if
-    python ../src/oxDNA_analysis_tools/align.py minitraj.dat aligned.dat 2>&1 >/dev/null | grep -y "ERROR"
+echo "Testing oat"
+echo "-----------"
+
+if [ $# -gt 0 ];
 then
-    echo "AN ERROR OCCURED"
+    PYTHON=$1
 else
-    echo "OK"
+    PYTHON="python"
 fi
 
-echo ""
+test() {
+    tmpfile=$(mktemp)
+    $PYTHON $1 &> $tmpfile
+    local code_res=$?
+    grep_out=$(grep -i "ERROR" $tmpfile)
+    local grep_res=$?
 
-echo "Testing backbone_flexibility..."
-if
-    python ../src/oxDNA_analysis_tools/backbone_flexibility.py rna_tile.top minitraj.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
+    if [ $code_res -eq 0 ] && [ $grep_res -eq 1 ]
+    then
+        echo " OK"
+    else
+        echo ""
+        echo $grep_out
+        echo "--> AN ERROR OCCURRED <--"
+    fi
+}
 
-echo ""
+echo -n "Testing align..."
+test "../src/oxDNA_analysis_tools/align.py minitraj.dat aligned.dat"
 
-echo "Testing bond_analysis..."
-if
-    python ../src/oxDNA_analysis_tools/bond_analysis.py -p2 input_rna minitraj.dat pairs.txt 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
+echo -n "Testing backbone_flexibility..."
+test "../src/oxDNA_analysis_tools/backbone_flexibility.py rna_tile.top minitraj.dat"
 
-echo ""
+echo -n "Testing bond_analysis..."
+test "../src/oxDNA_analysis_tools/bond_analysis.py -p2 input_rna minitraj.dat pairs.txt"
 
-echo "Testing mean and deviations..."
-if
-    python  ../src/oxDNA_analysis_tools/mean.py -p 2 -d devs.json minitraj.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
+echo -n "Testing mean and deviations..."
+test "../src/oxDNA_analysis_tools/mean.py -p 2 -d devs.json minitraj.dat"
 
-echo ""
+echo -n "Testing centroid with indexing..."
+test "../src/oxDNA_analysis_tools/centroid.py -i index.txt mean.dat minitraj.dat"
 
-echo "Testing centroid with indexing..."
-if
-    python ../src/oxDNA_analysis_tools/centroid.py -i index.txt mean.dat minitraj.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
+echo -n "Testing contact_map..."
+test "../src/oxDNA_analysis_tools/contact_map.py minitraj.dat"
 
-echo ""
+echo -n "Testing distance and clustering (this one takes a while because of the plot)..."
+test "../src/oxDNA_analysis_tools/distance.py -c -i minitraj.dat 1 3 5 67 34 56"
 
-echo "Testing contact_map..."
-if
-    python ../src/oxDNA_analysis_tools/contact_map.py minitraj.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
+echo -n "Testing duplex_finder..."
+test "../src/oxDNA_analysis_tools/duplex_finder.py input_rna minitraj.dat"
 
-echo ""
+echo -n "Testing duplex_angle_plotter..."
+test "../src/oxDNA_analysis_tools/duplex_angle_plotter.py -o angle.png -i angles.txt 7 37"
 
-echo "Testing distance and clustering (this one takes a while because of the plot)..."
-if
-    python ../src/oxDNA_analysis_tools/distance.py -c -i minitraj.dat 1 3 5 67 34 56 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
+echo -n "Testing generate_force..."
+test "../src/oxDNA_analysis_tools/generate_force.py -f gen_pairs.txt input_rna minitraj.dat"
 
-echo ""
+echo -n "Testing minify..."
+test "../src/oxDNA_analysis_tools/minify.py -a -d2 minitraj.dat min.dat"
 
-echo "Testing duplex_finder..."
-if
-    python ../src/oxDNA_analysis_tools/duplex_finder.py input_rna minitraj.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
+echo -n "Testing multidimensional_scaling_mean..."
+test "../src/oxDNA_analysis_tools/multidimensional_scaling_mean.py minitraj.dat -o meanM -d devsM"
 
-echo ""
+echo -n "Testing output_bonds..."
+test "../src/oxDNA_analysis_tools/output_bonds.py -v energy.json input_rna minitraj.dat"
 
-echo "Testing duplex_angle_plotter..."
-if
-    python ../src/oxDNA_analysis_tools/duplex_angle_plotter.py -o angle.png -i angles.txt 7 37 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
+echo -n "Testing pca"
+test "../src/oxDNA_analysis_tools/pca.py minitraj.dat mean.dat pca.json"
 
-echo ""
+echo -n "Testing subset_trajectory..."
+test "../src/oxDNA_analysis_tools/subset_trajectory.py minitraj.dat rna_tile.top -i index.txt sub.dat"
 
-echo "Testing generate_force..."
-if
-    python ../src/oxDNA_analysis_tools/generate_force.py -f gen_pairs.txt input_rna minitraj.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
-
-echo ""
-
-echo "Testing minify..."
-if
-    python ../src/oxDNA_analysis_tools/minify.py -a -d2 minitraj.dat min.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
-
-echo ""
-
-echo "Testing multidimensional_scaling_mean..."
-if
-    python ../src/oxDNA_analysis_tools/multidimensional_scaling_mean.py minitraj.dat -o meanM -d devsM 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
-
-echo ""
-
-echo "Testing output_bonds..."
-if
-    python ../src/oxDNA_analysis_tools/output_bonds.py -v energy.json input_rna minitraj.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
-
-echo ""
-
-echo "Testing pca"
-if
-    python ../src/oxDNA_analysis_tools/pca.py minitraj.dat mean.dat pca.json 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
-
-echo "Testing subset_trajectory..."
-if
-    python ../src/oxDNA_analysis_tools/subset_trajectory.py minitraj.dat rna_tile.top -i index.txt sub.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
-
-echo ""
-
-echo "Testing superimpose..."
-if
-    python ../src/oxDNA_analysis_tools/superimpose.py mean.dat centroid.dat 2>&1 >/dev/null | grep -y "ERROR"
-then
-    echo "AN ERROR OCCURED"
-else
-    echo "OK"
-fi
+echo -n "Testing superimpose..."
+test "../src/oxDNA_analysis_tools/superimpose.py mean.dat centroid.dat"
 
 echo ""
