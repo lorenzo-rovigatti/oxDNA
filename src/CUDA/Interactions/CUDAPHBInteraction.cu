3566caab4ed59ae48b1a963b138db2d3ad27c7f9
Subhajit Roy <me.subhajitroy1999@gmail.com>
2024-02-15 12:22:09 -0700

    update

--- src/CUDA/Interactions/CUDAPHBInteraction.cu
+++ src/CUDA/Interactions/CUDAPHBInteraction.cu
@@ -1,1 +1,26 @@
 #include "CUDAPHBInteraction.cuh"
+
+
+CUDAPHBInteraction::CUDAPHBInteraction() {
+    _edge_compatible = true;
+}
+
+CUDAPHBInteraction::~CUDAPHBInteraction() {
+}
+
+void CUDAPHBInteraction::get_settings(input_file &inp) {
+    PHBInteraction::get_settings(inp);
+}
+
+void CUDAPHBInteraction::get_cuda_settings(input_file &inp) {
+    PHBInteraction::get_settings(inp);
+}
+
+void CUDAPHBInteraction::cuda_init(int N){
+    CUDABaseInteraction::cuda_init(N);
+    PHBInteraction::init();
+}
+
+void CUDAPHBInteraction::compute_forces() {
+    
+}
