b1eb83d3ef53e4ec93127aa30a851640d315b65f
Subhajit Roy <me.subhajitroy1999@gmail.com>
2024-03-08 21:16:28 -0700

    workbreak

---
+++ src/CUDA/Interactions/CUDAPSP.cu
@@ -0,0 +1,4 @@
+// Main interaction for CUDA Patchy Spring Particles
+
+
+#include "CUDAPSP.cuh"
---
+++ src/CUDA/Interactions/CUDAPSP.cuh
@@ -0,0 +1,13 @@
+// CUDA Patchy Spring Particles
+
+#ifndef CUDA_PSP_CUH
+#define CUDA_PSP_CUH
+
+#include "CUDABaseInteractions.h"
+#include "../cuda_utils/CUDA_lr_common.cuh"
+#include "../Lists/CUDASimpleVerletList.h"
+#include "../Lists/CUDANoList.h"
+
+
+
+#endif // CUDA_PSP_CUH
