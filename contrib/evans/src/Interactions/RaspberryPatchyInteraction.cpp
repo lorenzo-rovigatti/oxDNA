bfb393f3b4146ac2cb35d1f62b3c5f6454956a93
Josh Evans <joshuaevanslowell@gmail.com>
2024-12-03 14:30:13 -0700

    created "evans" contributor directory. created CMakeLists.txt file. created files for "RaspberryPatchyInteraction". name is "Temporary" but there's nothing as permenant as a temporary solution.... cpp and h files are empty as of this commit. CMakeLists.txt should have build target configured

---
+++ contrib/evans/src/Interactions/RaspberryPatchyInteraction.cpp
@@ -0,0 +1,8 @@
+//
+// Created by josh evans on 3 December 2024
+// Name is temporary (aka, we're going to keep saying we should get around to renaming it, then not do that)
+// code will be pilfered (sorry, "adapted") from both romano/src/Interactions/PatchyShapeInteraction (on oxDNA_torsion repo)
+// and a little from Lorenzo Rovigatti's interactions
+//
+
+#include "RaspberryPatchyInteraction.h"
