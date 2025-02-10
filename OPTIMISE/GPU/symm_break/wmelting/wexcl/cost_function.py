Increased mesh points in computation of acos(th4) in f4th4 crst. Approximation of acos was not precise! Fixed something in reweighting of F2 of crst that was preventing using r0 of crst as opti-parameter

OPTIMISE/GPU/symm_break/wmelting/wexcl/cost_function.py
@@ -956,6 +956,13 @@ def build_masks_and_symm_tensors() :
     SYMM_LIST = torch.tensor(sl,device=device)
     SYMM_LIST_SYMM = torch.tensor(sls,device=device)
 
+    print("UPDAE MAP:")
+    print(UPDATE_MAP)
+    print("SYMM_LIST:")
+    print(SYMM_LIST)
+    print("SYMM_LIST_SYMM:")
+    print(SYMM_LIST_SYMM)
+
 
     # Nota:
     # stacking - qn3, q, p, pn5
@@ -1184,6 +1191,19 @@ def compute_initial_energy() :
     CRST_33_MOD_IN  = f2*f4_th4
     CRST_33_MOD_FIX  = f4_th1*f4_th2*f4_th3*f4_th7*f4_th8
 
+    """
+    mfile = open("CRST_F2_33.txt", 'w')
+    print(f2[0][0], file=mfile)
+    mfile.close()
+
+    mfile = open("CRST_F4TH4_33.txt", 'w')
+    print(f4_th4[0][0], file=mfile)
+    mfile.close()
+
+    mfile = open("TH4_UNBN.txt", 'w')
+    print(TH4_UNBN[0][0], file=mfile)
+    mfile.close()
+    """
 
     #55
     f2 = F2(HYDR_R, PARS_IN[par_index[116]][TYPES_UNBN_55], PARS_IN[par_index[117]][TYPES_UNBN_55], PARS_IN[par_index[118]][TYPES_UNBN_55], PARS_IN[par_index[119]][TYPES_UNBN_55], PARS_IN[par_index[120]][TYPES_UNBN_55],\
@@ -1207,6 +1227,16 @@ def compute_initial_energy() :
     f4_th8 = F4(TH8,PARS_IN[par_index[150]][TYPES_UNBN_55],PARS_IN[par_index[151]][TYPES_UNBN_55],PARS_IN[par_index[152]][TYPES_UNBN_55],\
                 PARS_IN[par_index[153]][TYPES_UNBN_55],PARS_IN[par_index[154]][TYPES_UNBN_55],ZEROS_UNBN)
 
+    """
+    mfile = open("CRST_F2_55.txt", 'w')
+    print(f2[0][0], file=mfile)
+    mfile.close()
+
+    mfile = open("CRST_F4TH4_55.txt", 'w')
+    print(f4_th4[0][0], file=mfile)
+    mfile.close()
+    """
+
     EN_CRST_55_IN = f2*f4_th1*f4_th2*f4_th3*f4_th4*f4_th7*f4_th8
     CRST_55_MOD_IN = f2*f4_th4
     CRST_55_MOD_FIX  = f4_th1*f4_th2*f4_th3*f4_th7*f4_th8
@@ -1222,9 +1252,9 @@ def compute_rew_factor(PARS,SH_ST) :
 
 
     #EXCL_BN
-    EN_EXCL_BN_REW = F3(EXCL_BA_BA_R_BN,PARS_IN[par_index[171]][TYPES_BN],PARS_IN[par_index[172]][TYPES_BN],PARS_IN[par_index[173]][TYPES_BN],PARS_IN[par_index[174]][TYPES_BN]) +\
-                     F3(EXCL_BA_BB_R_BN,PARS_IN[par_index[175]][TYPES_BN],PARS_IN[par_index[176]][TYPES_BN],PARS_IN[par_index[177]][TYPES_BN],PARS_IN[par_index[178]][TYPES_BN]) +\
-                     F3(EXCL_BB_BA_R_BN,PARS_IN[par_index[179]][TYPES_BN],PARS_IN[par_index[180]][TYPES_BN],PARS_IN[par_index[181]][TYPES_BN],PARS_IN[par_index[182]][TYPES_BN])
+    EN_EXCL_BN_REW = F3(EXCL_BA_BA_R_BN,PARS[par_index[171]][TYPES_BN],PARS[par_index[172]][TYPES_BN],PARS[par_index[173]][TYPES_BN],PARS[par_index[174]][TYPES_BN]) +\
+                     F3(EXCL_BA_BB_R_BN,PARS[par_index[175]][TYPES_BN],PARS[par_index[176]][TYPES_BN],PARS[par_index[177]][TYPES_BN],PARS[par_index[178]][TYPES_BN]) +\
+                     F3(EXCL_BB_BA_R_BN,PARS[par_index[179]][TYPES_BN],PARS[par_index[180]][TYPES_BN],PARS[par_index[181]][TYPES_BN],PARS[par_index[182]][TYPES_BN])
 
     #FENE_ENERGY+CONTINUITY-TO AVOID DIVERGENT ENERGY
 
@@ -1271,8 +1301,8 @@ def compute_rew_factor(PARS,SH_ST) :
     #CROSS STACKING
     #33
 
-    f2 = F2(HYDR_R, PARS_IN[par_index[77]][TYPES_UNBN_33], PARS_IN[par_index[78]][TYPES_UNBN_33], PARS_IN[par_index[79]][TYPES_UNBN_33], PARS_IN[par_index[80]][TYPES_UNBN_33], PARS_IN[par_index[81]][TYPES_UNBN_33],\
-            PARS_IN[par_index[82]][TYPES_UNBN_33], PARS_IN[par_index[83]][TYPES_UNBN_33], PARS_IN[par_index[84]][TYPES_UNBN_33], PARS_IN[par_index[85]][TYPES_UNBN_33], ZEROS_UNBN)
+    f2 = F2(HYDR_R, PARS[par_index[77]][TYPES_UNBN_33], PARS[par_index[78]][TYPES_UNBN_33], PARS[par_index[79]][TYPES_UNBN_33], PARS[par_index[80]][TYPES_UNBN_33], PARS[par_index[81]][TYPES_UNBN_33],\
+            PARS[par_index[82]][TYPES_UNBN_33], PARS[par_index[83]][TYPES_UNBN_33], PARS[par_index[84]][TYPES_UNBN_33], PARS[par_index[85]][TYPES_UNBN_33], ZEROS_UNBN)
 
     f4_th4 = F4(TH4_UNBN,PARS[par_index[101]][TYPES_UNBN_33],PARS[par_index[102]][TYPES_UNBN_33],PARS[par_index[103]][TYPES_UNBN_33],\
                 PARS[par_index[104]][TYPES_UNBN_33],PARS[par_index[105]][TYPES_UNBN_33],ZEROS_UNBN)
@@ -1280,8 +1310,8 @@ def compute_rew_factor(PARS,SH_ST) :
     CRST_33_MOD_REW = f2*f4_th4
 
 
-    f2 = F2(HYDR_R, PARS_IN[par_index[116]][TYPES_UNBN_55], PARS_IN[par_index[117]][TYPES_UNBN_55], PARS_IN[par_index[118]][TYPES_UNBN_55], PARS_IN[par_index[119]][TYPES_UNBN_55], PARS_IN[par_index[120]][TYPES_UNBN_55],\
-            PARS_IN[par_index[121]][TYPES_UNBN_55], PARS_IN[par_index[122]][TYPES_UNBN_55], PARS_IN[par_index[123]][TYPES_UNBN_55], PARS_IN[par_index[124]][TYPES_UNBN_55], ZEROS_UNBN)
+    f2 = F2(HYDR_R, PARS[par_index[116]][TYPES_UNBN_55], PARS[par_index[117]][TYPES_UNBN_55], PARS[par_index[118]][TYPES_UNBN_55], PARS[par_index[119]][TYPES_UNBN_55], PARS[par_index[120]][TYPES_UNBN_55],\
+            PARS[par_index[121]][TYPES_UNBN_55], PARS[par_index[122]][TYPES_UNBN_55], PARS[par_index[123]][TYPES_UNBN_55], PARS[par_index[124]][TYPES_UNBN_55], ZEROS_UNBN)
 
     f4_th4 = F4(TH4_UNBN,PARS[par_index[140]][TYPES_UNBN_55],PARS[par_index[141]][TYPES_UNBN_55],PARS[par_index[142]][TYPES_UNBN_55],\
                 PARS[par_index[143]][TYPES_UNBN_55],PARS[par_index[144]][TYPES_UNBN_55],ZEROS_UNBN)
@@ -1356,7 +1386,7 @@ def COST(PARS) :
     #ENSLAVED PARAMETERS
 
     #crst r0 ####
-    #crst_r0 = sqrt( stck_r0^2+hydr_r0^2/2*(1+cos(2asin(sqrt(fene_ro^2-stck_r0^2)))) )
+    #crst_r0 = sqrt( stck_r0^2+hydr_r0^2/2*(1+cos(2*asin(sqrt(fene_ro^2-stck_r0^2)))) )
 
     #We include this in the optimisation
 

OPTIMISE/GPU/symm_break/wmelting/wexcl/functions.py
@@ -6,7 +6,7 @@ import config as cg
 import cost_function as cfun
 import matplotlib.pyplot as plt
 import torch
-
+import cmath
 
 PARS_LIST = parl.PARS_LIST
 par_index = parl.par_index
@@ -1064,7 +1064,8 @@ def read_oxdna_trajectory_dist_and_angles(rcut_low, rcut_high, tr_file, topo_dat
                             prod = np.dot(n1.n,n2.n)
                             if prod > 1: prod -= 1e-12  #this is for the 0th configurations
                             if prod < -1: prod += 1e-12
-                            th4_unbn_1conf.append(np.arccos(prod))
+                            #th4_unbn_1conf.append(np.arccos(prod))
+                            th4_unbn_1conf.append(cmath.acos(prod).real)
                             th8_1conf.append(np.arccos(-np.dot(n1.n,rhydr)))
                             th7_1conf.append(np.arccos(np.dot(n2.n,rhydr)))
 

OPTIMISE/GPU/symm_break/wmelting/wexcl/optimise_geometry_gpu.py
@@ -335,8 +335,6 @@ cfun.print_initial_energy()
 
 print(fene_r[0][0])
 
-excl_ba_ba_r_bn
-
 """
 print("conf 79")
 for l in range(len(types_bn[0][80])) :
@@ -399,9 +397,6 @@ OPTI_PAR = torch.gather(torch.reshape(cfun.CURR_PARS,(-1,)),0,IDS_OP) #tensor wi
 
 print(cfun.OPT_PAR_LIST)
 
-print(cfun.CURR_PARS_m[78])
-print(cfun.CURR_PARS_m[117])
-
 print("OPTIMISING")
 
 #We use scipy to handle the optimisation algorithm
@@ -421,18 +416,18 @@ up_bond = torch.tensor(TMP, device='cpu').numpy()
 
 for n in range(len(low_bond)) :
     if cfun.OPT_PAR_LIST[n][0] == 1:
-        lb = low_bond[n]*0.98
-        ub = up_bond[n]*1.02
+        lb = low_bond[n]*0.97
+        ub = up_bond[n]*1.03
         if lb > 0.6: low_bond[n] = lb
         else: low_bond[n] = 0.6
         if ub > 0.9: up_bond[n] = ub
         else: up_bond[n] = 0.9
     elif cfun.OPT_PAR_LIST[n][0] == 78 or cfun.OPT_PAR_LIST[n][0] == 117:
-        low_bond[n] = low_bond[n]*0.98
-        up_bond[n] = up_bond[n]*1.02
+        low_bond[n] = low_bond[n]*0.97
+        up_bond[n] = up_bond[n]*1.03
     elif cfun.OPT_PAR_LIST[n][0] == 2:
-        low_bond[n] = low_bond[n]*0.98
-        up_bond[n] = up_bond[n]*1.02
+        low_bond[n] = low_bond[n]*0.97
+        up_bond[n] = up_bond[n]*1.03
     elif cfun.OPT_PAR_LIST[n][0] == 45:
         lb = low_bond[n]*0.98
         ub = up_bond[n]*1.02
@@ -472,12 +467,41 @@ bnd = optimize.Bounds(low_bond,up_bond)
 cfun.AVE_DELTA = torch.mean(cfun.CURR_PARS_m[2])
 print("Ave delta:", cfun.AVE_DELTA)
 
+print("Initial parameters:")
+counts = 0
+string = ""
+while counts < len(X0) :
+    string += "{:.4f}".format(X0[counts]) + " "
+    if counts % 20 == 0 :
+        print(string)
+        string = ""
+    counts += 1
+
+
+
+tfile = open("PARS_TEST.txt", 'w')
+for i in range(256) :
+    string = type_to_base4(i) + " " + str(float(cfun.CURR_PARS[101][i])) + " " + str(float(cfun.CURR_PARS[102][i])) + " " + str(float(cfun.CURR_PARS[103][i])) + " " + str(float(cfun.CURR_PARS[104][i])) + " " + str(float(cfun.CURR_PARS[105][i]))
+    print(string, file=tfile)
+tfile.close()
+
 print("S0: "+str(cfun.COST(X0)))
 print("lb0: ")
 print(cfun.LB)
 print("lt0: ")
 print(cfun.LT)
 
+"""
+print("Test1:")
+X0 += 0.02
+print("S0: "+str(cfun.COST(X0)))
+
+print("Test2:")
+X0 -= 0.04
+print("S0: "+str(cfun.COST(X0)))
+
+X0 += 0.02
+"""
 
 #callback is called at the end of every geometry opti iteration step
 #it perform the melting optimisation
@@ -486,7 +510,28 @@ def Callback(sol):
     tmp = []
     for i in range(len(sol)):
         tmp.append(sol[i]/X0[i])
-    print(tmp)
+    counts = 0
+    string = ""
+    while counts < len(sol) :
+        string += "{:.4f}".format(tmp[counts]) + " "
+        if counts % 20 == 0 :
+            print(string)
+            string = ""
+        counts += 1
+
+    print(string)
+
+    counts = 0
+    string = ""
+    while counts < len(sol) :
+        string += "{:.4f}".format(sol[counts]) + " "
+        if counts % 20 == 0 :
+            print(string)
+            string = ""
+        counts += 1
+
+    print(string)
+
     print("S: "+str(cfun.COST(sol)))
     print("lb: ")
     print(cfun.LB)
@@ -500,7 +545,7 @@ def Callback(sol):
 time_opti_start = time.time()
 
 ####### THIS LINE RUNS THE OPTIMISAION #######################
-sol = optimize.minimize(cfun.COST,X0, method='L-BFGS-B', callback=Callback, bounds=bnd, options={'maxiter':5,'iprint': 1})
+sol = optimize.minimize(cfun.COST,X0, method='L-BFGS-B', callback=Callback, bounds=bnd, options={'maxiter':8,'iprint': 1})
 
 S = cfun.COST(sol.x)
 print("Final value of the cost function: "+str(S))